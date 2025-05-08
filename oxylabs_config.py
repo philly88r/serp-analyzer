ba69d1f610426b249c86e4051bfc933fb14b0705
User <pematthews89@gmail.com>
2025-05-07 21:09:57 -0400

    Add Oxylabs credentials and configuration

---
+++ oxylabs_config.py
@@ -0,0 +1,19 @@
+# Oxylabs Configuration
+# Credentials filled in based on user's information
+
+# Proxy credentials
+OXYLABS_USERNAME = "customer-pematthews41_5eo28"
+OXYLABS_PASSWORD = "Yitbos88++88"
+
+# Proxy endpoint (based on user's information)
+# For residential proxies
+PROXY_URL = "us-pr.oxylabs.io:10000"
+
+# For SERP API (if you're using it instead of proxies)
+SERP_API_URL = "https://realtime.oxylabs.io/v1/queries/google/search"
+
+# Proxy type - set to "residential" since that's what the user has
+PROXY_TYPE = "residential"
+
+# Additional settings
+COUNTRY = "us"  # Target country for proxies
